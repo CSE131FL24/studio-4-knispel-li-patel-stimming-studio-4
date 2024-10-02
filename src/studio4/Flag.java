75d0f6e0ab3fcc79fbbc34716a835f006f7dfc79
Hopson <Hopson@10.232.161.227>
2024-10-02 14:08:32 -0500

    doug face flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		//new draw = StdDraw System(StdDraw);
+		StdDraw.setPenColor(60,120,200);
+		StdDraw.filledRectangle(.5, .5, .35, .2);
+	//	StdDraw.setPenColor(200,200,50);
+	//	StdDraw.filledCircle(.35,.55,.05);
+	//	StdDraw.filledCircle(.35,.45,.05);
+	//	StdDraw.filledEllipse(.5, .5, .15, .03);
+	//	StdDraw.setPenColor(20,60,20);
+		StdDraw.picture(.5, .5, "src/studio4/DOUG.png", 0.6, 0.4	);
 	}
+	
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,4 +1,4 @@
-package studio4;
+	package studio4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,6 +19,43 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		System.out.print(shape);
+		int color1 = in.nextInt();
+		int color2 = in.nextInt();
+		int color3 = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double v1 = in.nextDouble();
+		double v2 = in.nextDouble();
+		double v3 = in.nextDouble();
+		double v4 = in.nextDouble();
+	
+		StdDraw.setPenColor(color1,color2,color3);
+		if (shape .equals("rectangle")) {
+			//System.out.print("squrea");
+			if (isFilled == true) {
+				StdDraw.filledRectangle(v1,v2,v3,v4);
+			//	System.out.print("poP");
+		}
+			else if (isFilled == false) {
+				StdDraw.rectangle(v1, v2, v3, v4);
+			}
+		}
+		else if (shape .equals("triangle")) { 
+			double v5 = in.nextDouble();
+			double v6 = in.nextDouble();
+			double[] x = {v1,v2,v3};
+			double [] y = {v4,v5,v6};
+			if (isFilled == true) {
+				StdDraw.filledPolygon(x,y);
+			//	System.out.print("poP");
+		}
+			else if (isFilled == false) {
+				StdDraw.polygon(x,y);
+		}
+		}
+		
 		
 	}
 }
